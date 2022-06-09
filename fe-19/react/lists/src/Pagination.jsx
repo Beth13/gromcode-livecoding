180fa87d4832b712e2e0f6908cf47bc1cc8d7adf
JeePik <andrewlegeza@gmail.com>
2022-06-09 16:07:02 +0300

    updated files lists LC

--- fe-19/react/lists/src/Pagination.jsx
+++ fe-19/react/lists/src/Pagination.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 
 const Pagination = ({ goPrev, goNext, currentPage, totalItems, itemsPerPage }) => {
   const totalStr = Math.ceil(totalItems / itemsPerPage);
-  const isPrevPageAvailable = currentPage === 1;
-  const isNextPageAvailable = currentPage === totalStr;
+  const isPrevPageAvailable = currentPage !== 1;
+  const isNextPageAvailable = currentPage !== totalStr;
 
   return (
     <div className="pagination">
-      <button className="btn" disabled={isPrevPageAvailable ? 'disabled' : null} onClick={goPrev}>
-        {isPrevPageAvailable ? '' : '←'}
+      <button className="btn" disabled={!isPrevPageAvailable} onClick={goPrev}>
+        {!isPrevPageAvailable ? '' : '←'}
       </button>
       <span className="pagination__page">{currentPage}</span>
-      <button className="btn" disabled={isNextPageAvailable ? 'disabled' : null} onClick={goNext}>
-        {isNextPageAvailable ? '' : '→'}
+      <button className="btn" disabled={!isNextPageAvailable} onClick={goNext}>
+        {!isNextPageAvailable ? '' : '→'}
       </button>
     </div>
   );
